d02657a7e0283c952d97e9f387537a9e198eb554
Peter Oluwasegun <petersheg@gmail.com>
2021-09-20 11:30:21 +0100

    Debugging

--- controller/errorController/validation.js
+++ controller/errorController/validation.js
@@ -56,6 +56,8 @@ module.exports = (err,req,res,next)=>{
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'fail';
     
+    console.log(err);
+
     if(process.env.NODE_ENV === 'development'){
         console.log(err.message)
         developmentError(err,res)
