c1c5f7bfe9d6fedfa242c856ea08a4627e67cd20
Peter Oluwasegun <petersheg@gmail.com>
2021-10-15 18:04:14 +0100

    debugging

--- controller/webhook/paystack.js
+++ controller/webhook/paystack.js
@@ -2,52 +2,71 @@ const crypto = require('crypto');
 const secret = process.env.PAYSTACK_SECRET_KEY;
 const logger = require('../../utility/logger');
 const User = require('../../model/userModel');
+const Services = require('../../services/main');
 
 
 exports.webhookURL = async (req,res)=>{
     //validate event
     const hash = crypto.createHmac('sha512', secret).update(JSON.stringify(req.body)).digest('hex');
-    let event;
 
     if (hash == req.headers['x-paystack-signature']) {
+
      // Retrieve the request's body
-        event = req.body;
+        let event = req.body;
+
      // Do something with event 
-     const data = {
-        reference : event.data.reference,
-        email : event.data.customer.email
-     }
-     const currentUser = await User.findOne({userEmail : data.email});
-     
-     
+        const data = {
+
+            reference : event.data.reference,
+            email : event.data.customer.email,
+
+            headers : {
+                authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+                'content-type': 'application/json'
+            },
+
+            transactionObject = {
+                type : 'credit',
+                status : 'successful',
+                reason : 'fund wallet',
+                user : currentUser.id
+            }
+        }
+        const currentUser = await User.findOne({userEmail : data.email});
+        
+        const service = new Services(data.transactionObject,currentUser);
+        let verifyResponse,verified;
+
+        if (event.data.status === "success"){
+
+            // VERIFY THE TRANSACTION BY MAKING A GET CALL
+            const reference = data.reference;
+            const verifyURL = process.env.PAYSTACK_VERIFY_URL.replace(':reference',reference);
+
+            verifyResponse = await service.verifyPayStack(data.headers,verifyURL);
+
+            verified = verifyResponse.data.status && verifyResponse.data.data.status === "success";
+
+            // if( verified ){
+            //     const requestedAmount = verifyResponse.data.data.amount/toBaseCurrency;
+            //     await service.creditWallet(requestedAmount);
+            //     currentUser.save({validateBeforeSave : false});
+
+            // }else{
+
+            //     logger.Report({
+            //         service : 'controller::fundWallet',
+            //         message : 'Payment is abandoned',
+            //     })
+            // }
+        }
+
+        console.log(verified);
+        
         logger.Report({
             service : "controller::webhook::payStack",
-            message : JSON.stringify(currentUser)
+            message : JSON.stringify(verifyResponse)
         })
-
-
-        // if (event.data.status === "success"){
-
-        //     // VERIFY THE TRANSACTION BY MAKING A GET CALL
-        //     const reference = event.data.reference;
-        //     const verifyURL = process.env.PAYSTACK_VERIFY_URL.replace(':reference',reference);
-
-        //     const verifyResponse = await service.verifyPayStack(headers,verifyURL);
-        //     let verified = verifyResponse.data.status && verifyResponse.data.data.status === "success";
-
-        //     if( verified ){
-        //         const requestedAmount = verifyResponse.data.data.amount/toBaseCurrency;
-        //         await service.creditWallet(requestedAmount);
-        //         currentUser.save({validateBeforeSave : false});
-
-        //     }else{
-
-        //         logger.Report({
-        //             service : 'controller::fundWallet',
-        //             message : 'Payment is abandoned',
-        //         })
-        //     }
-        // }
 
     }
     res.send(200);
