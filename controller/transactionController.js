edc05b728196dd2022546eca147ef496617c7ad6
Peter Oluwasegun <petersheg@gmail.com>
2021-11-01 16:53:40 +0100

    Implented pagination for user transaction

--- controller/transactionController.js
+++ controller/transactionController.js
@@ -1,6 +1,7 @@
 const catchAsync = require('../utility/catchAsync');
 const AppError = require('../utility/appError');
 const Transaction = require('../model/transaction');
+let _ = require('lodash');
 
 exports.getTransactionByUser = catchAsync(
 
@@ -22,11 +23,26 @@ exports.getTransactionByUser = catchAsync(
         if(userId  || req.query){
             filter = {user : userId, ...req.query};
         }
-        
-        const transactions = await Transaction.find(filter);
+        let newFilter = _.omit(filter,["page","limit"]);
+        let transaction = Transaction.find(newFilter);
+
+        // Implement Pagination
+        const pages = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 3;
+        const skip = (pages - 1) * limit;
+
+        transaction = transaction.skip(skip).limit(limit);
+
+        // if(req.query.page){
+        //     const allTransactions = await Transaction.countDocuments();
+        //     if(skip > allTransactions){
+        //         return next(new AppError("Requested page is not available"))
+        //     }
+        // }
+        const transactions = await transaction;
 
         if(!transactions || transactions.length === 0){
-            return next(new AppError('No transaction(s) for this user',404));
+            return next(new AppError('Transaction(s) not available',404));
         }
         
         res.status(200).json({
