fa86f24697d0077a034dd39d5c6ef7ed25cc6ad8
Peter Oluwasegun <petersheg@gmail.com>
2022-08-10 18:10:09 +0100

    added heroku git

--- controller/contact.js
+++ controller/contact.js
@@ -2,7 +2,7 @@ const catchAsync = require('../utility/catchAsync');
 const AppError = require('../utility/appError');
 const sendAnyEmail = require('../utility/emails/sendGrid');
 const User = require('../model/user');
-
+const {sentToWhatsApp} = require('../utility/whatsApp/twilio');
 
 exports.contact = catchAsync(
 
@@ -12,7 +12,7 @@ exports.contact = catchAsync(
         if(!email || !name || !message){
             return next(new AppError("All field are required",400));
         }
-        await User.create({name,email});
+        // await User.create({name,email});
         const options = {
             subject : `Message from ${name}`,
             message: `
@@ -21,7 +21,8 @@ exports.contact = catchAsync(
         }
 
         try{
-            await sendAnyEmail(options);
+            // await sendAnyEmail(options);
+            await sentToWhatsApp(message);
 
            res.status(200).json({
                 status : 'success',
---
+++ utility/whatsApp/twilio.js
@@ -0,0 +1,16 @@
+let accountSid = process.env.TWILIO_ACCOUNT_ID; // Your Account SID from www.twilio.com/console
+let authToken = process.env.TWILIO_AUTH_TOKEN;   // Your Auth Token from www.twilio.com/console
+
+const client = require('twilio')(accountSid, authToken);
+
+
+exports.sentToWhatsApp = async(message)=> {
+    client.messages 
+      .create({ 
+         body: message, 
+         from: 'whatsapp:+14155238886',       
+         to: 'whatsapp:+2348132665626' 
+       }) 
+      .then(message => console.log(message.sid)) 
+      .done();
+}
